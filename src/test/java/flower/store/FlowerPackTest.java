855eb9bd5e77571ca235f9e1a25056e991067403
Veronika Bahatyr-Zakharchenko <bahatyr.pn@ucu.edu.ua>
2024-10-12 20:20:13 +0300

    flower pack remade

--- src/test/java/flower/store/FlowerPackTest.java
+++ src/test/java/flower/store/FlowerPackTest.java
@@ -1,32 +1,40 @@
 package flower.store;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
 
 public class FlowerPackTest {
 
+    private static final double ROSE_SEPAL_LENGTH = 12.0;
+    private static final double ROSE_PRICE = 5.0;
+    private static final int INITIAL_QUANTITY = 10;
+    private static final double TULIP_SEPAL_LENGTH = 8.0;
+    private static final double TULIP_PRICE = 7.0;
+    private static final int NEW_QUANTITY = 15;
+    private static final double DELTA = 0.01;
+
     @Test
     public void testCreateFlowerPack() {
-        Flower rose = new Rose(12.0, FlowerColor.RED, 5.0);
-        FlowerPack pack = new FlowerPack(rose, 10);
-        assertEquals(rose, pack.getFlower());
-        assertEquals(10, pack.getQuantity());
+        Flower rose = new Rose(ROSE_SEPAL_LENGTH, FlowerColor.RED, ROSE_PRICE);
+        FlowerPack pack = new FlowerPack(rose, INITIAL_QUANTITY);
+        Assertions.assertEquals(rose, pack.getFlower());
+        Assertions.assertEquals(INITIAL_QUANTITY, pack.getQuantity());
     }
 
     @Test
     public void testGetPrice() {
-        Flower tulip = new Tulip(8.0, FlowerColor.BLUE, 7.0);
+        Flower tulip = new Tulip(TULIP_SEPAL_LENGTH, FlowerColor.BLUE, TULIP_PRICE);
         FlowerPack pack = new FlowerPack(tulip, 5);
-        double expectedPrice = 7.0 * 5;
-        assertEquals(expectedPrice, pack.getPrice(), 0.01);
+        double expectedPrice = TULIP_PRICE * 5;
+        Assertions.assertEquals(expectedPrice, pack.getPrice(), DELTA);
     }
 
     @Test
     public void testSetQuantity() {
         Flower chamomile = new Flower(10.0, FlowerColor.RED, 3.0, FlowerType.CHAMOMILE);
-        FlowerPack pack = new FlowerPack(chamomile, 10);
-        pack.setQuantity(15);
-        assertEquals(15, pack.getQuantity());
+        FlowerPack pack = new FlowerPack(chamomile, INITIAL_QUANTITY);
+        pack.setQuantity(NEW_QUANTITY);
+        Assertions.assertEquals(NEW_QUANTITY, pack.getQuantity());
     }
 
     @Test
@@ -35,6 +43,6 @@ public class FlowerPackTest {
         Flower rose = new Rose(10.0, FlowerColor.RED, 5.0);
         FlowerPack pack = new FlowerPack(tulip, 5);
         pack.setFlower(rose);
-        assertEquals(rose, pack.getFlower());
+        Assertions.assertEquals(rose, pack.getFlower());
     }
 }

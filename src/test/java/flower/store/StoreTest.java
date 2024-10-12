d0cc45a251d782f2947594a950c1cae0ff169a83
Veronika Bahatyr-Zakharchenko <bahatyr.pn@ucu.edu.ua>
2024-10-12 20:21:31 +0300

    storetest remade

--- src/test/java/flower/store/StoreTest.java
+++ src/test/java/flower/store/StoreTest.java
@@ -2,29 +2,41 @@ package flower.store;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class StoreTest {
 
+    private static final double ROSE_SEPAL_LENGTH = 10.0;
+    private static final double ROSE_PRICE = 5.0;
+    private static final int ROSE_QUANTITY = 10;
+    private static final double TULIP_SEPAL_LENGTH = 8.0;
+    private static final double TULIP_PRICE = 7.0;
+    private static final int TULIP_QUANTITY = 5;
+
     @Test
     public void testSearch() {
         Store store = new Store();
-        Flower rose = new Rose(10, FlowerColor.RED, 5);
-        Flower tulip = new Tulip(8, FlowerColor.RED, 7);
-        FlowerPack rosePack = new FlowerPack(rose, 10);
-        FlowerPack tulipPack = new FlowerPack(tulip, 5);
-        FlowerBucket bucket1 = new FlowerBucket();
-        bucket1.add(rosePack);
-        FlowerBucket bucket2 = new FlowerBucket();
-        bucket2.add(tulipPack);
-        store.addBucket(bucket1);
-        store.addBucket(bucket2);
+
+        Flower rose = new Rose(ROSE_SEPAL_LENGTH, FlowerColor.RED, ROSE_PRICE);
+        Flower tulip = new Tulip(TULIP_SEPAL_LENGTH, FlowerColor.RED, TULIP_PRICE);
+
+        FlowerPack rosePack = new FlowerPack(rose, ROSE_QUANTITY);
+        FlowerPack tulipPack = new FlowerPack(tulip, TULIP_QUANTITY);
+
+        FlowerBucket roseBucket = new FlowerBucket();
+        roseBucket.add(rosePack);
+
+        FlowerBucket tulipBucket = new FlowerBucket();
+        tulipBucket.add(tulipPack);
+
+        store.addBucket(roseBucket);
+        store.addBucket(tulipBucket);
+
         List<FlowerBucket> result = store.search(FlowerType.ROSE);
-        assertEquals(1, result.size());
-        assertTrue(result.contains(bucket1));
-        assertFalse(result.contains(bucket2));
+
+        Assertions.assertEquals(1, result.size());
+        Assertions.assertTrue(result.contains(roseBucket));
+        Assertions.assertFalse(result.contains(tulipBucket));
     }
 }
